9d8ff4b1af36f725787be7e1cfe243abe11d207f
shivani <srivastavashivani1994@gmail.com>
2021-05-03 00:18:15 +0530

    Added linked list implementation of queue

---
+++ Queue.java
@@ -0,0 +1,62 @@
+public class Queue{
+
+    public class Node {
+
+        int data;
+        Node next;
+    
+
+        public Node (int data){
+        this.data = data;
+        this.next = null;
+        }
+    }
+
+Node head = null;
+Node tail = null;
+
+    public void enqueue(int data){
+
+        Node newNode = new Node (data);
+
+        if (head == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public int dequeue (){
+        if(head!=null){
+            int data = head.data;
+            head = head.next;
+            return data;
+        }else{
+
+            throw new ArithmeticException("not valid");  
+
+        }
+    }
+
+    public boolean isEmpty(){
+
+        if(head != null){
+            return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args){
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(4);
+        queue.enqueue(3);
+        System.out.println(queue.dequeue());
+
+        
+    }
+}
